8c9022bc95ec3ea6f82c21af7e3123e954125c4b
Tomáš Stankovič <hello@slinto.sk>
2016-01-29 21:55:17 +0100

    Pridany sitemap generator + robots.txt.

--- server/router.js
+++ server/router.js
@@ -1,10 +1,33 @@
 /**
  * App router.
  */
+var sitemap = require('express-sitemap');
+
 var setup = function(app) {
   var staticController = require('./controllers/staticController');
 
   app.use('/', staticController);
+
+  var map = sitemap({
+    url: 'www.webtlak.sk',
+    sitemap: 'data/sitemap.xml',
+    robots: 'data/robots.txt',
+    generate: app,
+    sitemapSubmission: '/sitemap.xml',
+    map: {
+      '/': ['get']
+    },
+    route: {
+      '/': {
+        changefreq: 'always',
+        priority: 1.0
+      }
+    }
+  });
+
+  map.generate(app);
+  map.XMLtoFile();
+
 };
 
 module.exports.setup = setup;

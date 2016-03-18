cdfed3d00ec049b3dbeeea6c14ac10366a9c8df9
Tomáš Stankovič <hello@slinto.sk>
2016-03-18 15:13:35 +0100

    Title uprava.

--- server/controllers/staticController.js
+++ server/controllers/staticController.js
@@ -9,7 +9,8 @@ var express = require('express'),
  */
 router.get('/', function(req, res) {
   res.render('static/index',{
-    FB_EVENT_URL: 'http://www.facebook.com/events/1252817861398596/'
+    FB_EVENT_URL: 'http://www.facebook.com/events/1252817861398596/',
+    title: 'WEBtlak #4 / 24.3.2016 - 19.00 / Záhrada CNK'
   });
 });
 

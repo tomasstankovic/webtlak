2606aceece5dc8f99ab5aac9f1896b76347a9b03
Tomáš Stankovič <hello@slinto.sk>
2016-07-06 15:57:46 +0200

    Intro + Archive: update

--- server/controllers/staticController.js
+++ server/controllers/staticController.js
@@ -9,8 +9,8 @@ var express = require('express'),
  */
 router.get('/', function(req, res) {
   res.render('static/index',{
-    FB_EVENT_URL: 'http://www.facebook.com/events/1721091454839582/',
-    title: 'WEBtlak #6 / 24.5.2016 (Utorok) - 19.00 / Záhrada CNK'
+    FB_EVENT_URL: 'http://www.facebook.com/webtlak/',
+    title: null
   });
 });
 
